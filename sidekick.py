3e24cee581f336753cfcdf3abf806723071f1b7d
Brian Rosner <brian@brosner.com>
2016-06-02 08:20:30 -0600

    added tracer bullet for database connection

--- sidekick.py
+++ sidekick.py
@@ -1,12 +1,17 @@
-from flask import Flask
+import psycopg2
+
+from flask import Flask, jsonify
 
 
 app = Flask(__name__)
 
 
 @app.route("/")
-def hello():
-    return "Hello World!"
+def index():
+    conn = psycopg2.connect(host="localhost", dbname="postgres", user="postgres")
+    cur = conn.cursor()
+    cur.execute("SELECT row_to_json(row(pg_postmaster_start_time()))")
+    return jsonify(cur.fetchone()[0])
 
 
 if __name__ == "__main__":
